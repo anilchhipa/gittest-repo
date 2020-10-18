56579c0f6b82e7f3d7d47d360ef00178f040a8ce
anilchhipa <33485395+anilchhipa@users.noreply.github.com>
2020-10-18 09:44:10 +0530

    Update GitTestManager.java
    Git is awesome add from remote 1

--- src/main/java/com/chhipa/git/GitTestManager.java
+++ src/main/java/com/chhipa/git/GitTestManager.java
@@ -3,6 +3,7 @@ package com.chhipa.git;
 public class GitTestManager {
 	public static void main(String[] args) {
 		System.out.println("Git is awesome add from local 1");
+		System.out.println("Git is awesome add from remote 1");
 		System.out.println("Git is awesome add from local 2");
 	}
 }
