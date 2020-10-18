899f690634d590206325bd7046478f7c4949b286
Anil Chhipa <anilchhipa@gmail.com>
2020-10-18 09:42:17 +0530

    Git is awesome add from local 2

--- src/main/java/com/chhipa/git/GitTestManager.java
+++ src/main/java/com/chhipa/git/GitTestManager.java
@@ -3,5 +3,6 @@ package com.chhipa.git;
 public class GitTestManager {
 	public static void main(String[] args) {
 		System.out.println("Git is awesome add from local 1");
+		System.out.println("Git is awesome add from local 2");
 	}
 }
