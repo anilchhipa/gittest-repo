2eb41f6b470f9a28fe73c80896048ad12965fff5
Anil Chhipa <anilchhipa@gmail.com>
2020-10-18 09:51:23 +0530

    Merge 1

--- src/main/java/com/chhipa/git/GitTestManager.java
+++ src/main/java/com/chhipa/git/GitTestManager.java
@@ -3,6 +3,8 @@ package com.chhipa.git;
 public class GitTestManager {
 	public static void main(String[] args) {
 		System.out.println("Git is awesome add from local 1");
+		System.out.println("Git is awesome add from remote 1");
 		System.out.println("Git is awesome add from local 2");
+		System.out.println("Git is awesome add from local 3");
 	}
 }
